0e0bf857e02d224d4a28805e52d04e8a54aa612b
RustamElkin <ryolkin90@gmail.com>
2021-02-15 17:53:16 +0600

    added to new pages xpath locators

--- src/main/java/DiplomaContingentPage.java
+++ src/main/java/DiplomaContingentPage.java
@@ -10,22 +10,22 @@ import static com.codeborne.selenide.Selenide.$x;
 public class DiplomaContingentPage {
 
     // Тайтл
-    private SelenideElement titleToPage           =$x("//h1[contains(text(),'Дипломные работы')]");
+    private SelenideElement titleToPage           = $x("//h1[contains(text(),'Дипломные работы')]");
 
     // Табы на странице
-    private SelenideElement backTab               =$x("//span[contains(text(),'Назад')]/..");                        // 'Назад'
+    private SelenideElement backTab               = $x("//span[contains(text(),'Назад')]/..");                        // 'Назад'
 
     // Получение списока строк в таблице
-    private ElementsCollection selectAllTd        =$$x("//table/tbody//tr/td[1]");
+    private ElementsCollection selectAllTd        = $$x("//table/tbody//tr/td[1]");
 
     // Можно проверить ФИО и группу студента
-    private SelenideElement fullNameAndGroupCheck =$x("//h3[contains(@class,'s-title--double-line s-upper s-light')]/span");
+    private SelenideElement fullNameAndGroupCheck = $x("//h3[contains(@class,'s-title--double-line s-upper s-light')]/span");
 
     // Таблица
-    private SelenideElement themeThesisWork       =$x("//td[@class='s-table__col--to-right' and text()= 'Тема дипломной работы:']/following-sibling::td");  // 'Тема дипломной работы'
-    private SelenideElement personnelName         =$x("//td[@class='s-table__col--to-right' and text()= 'Руководитель:']/following-sibling::td");           // 'Руководитель'
-    private SelenideElement protectionDate        =$x("//td[@class='s-table__col--to-right' and text()= 'Дата защиты :']/following-sibling::td");           // 'Дата защиты'
-    private SelenideElement point                 =$x("//td[@class='s-table__col--to-right' and text()= 'Оценка:']/following-sibling::td");                 // 'Оценка'
+    private SelenideElement themeThesisWork       = $x("//td[@class='s-table__col--to-right' and text()= 'Тема дипломной работы:']/following-sibling::td");  // 'Тема дипломной работы'
+    private SelenideElement personnelName         = $x("//td[@class='s-table__col--to-right' and text()= 'Руководитель:']/following-sibling::td");           // 'Руководитель'
+    private SelenideElement protectionDate        = $x("//td[@class='s-table__col--to-right' and text()= 'Дата защиты :']/following-sibling::td");           // 'Дата защиты'
+    private SelenideElement point                 = $x("//td[@class='s-table__col--to-right' and text()= 'Оценка:']/following-sibling::td");                 // 'Оценка'
 
 
 }
--- src/main/java/ViewAnnualArchiveExamPage.java
+++ src/main/java/ViewAnnualArchiveExamPage.java
@@ -1,8 +1,28 @@
 //Архив_Экзамен
 //  /ru/tko/control/annual-archive/modules/4/exams/447
 
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ViewAnnualArchiveExamPage {
 
+    // Тайтл
+    private SelenideElement titleToPage           = $x("//h1[contains(text(),'Экзамен')]");
 
+    // Табы на странице
+    private SelenideElement backTab               = $x("//span[contains(text(),'Назад')]/..");             // 'Назад'
+
+    // Тайтлы таблиц
+    private SelenideElement numberOnTable         = $x("//h3[contains(text(),'Общие сведения')]");         // 'Общие сведения'
+    private SelenideElement specialtyTable        = $x("//h3[contains(text(),'Участники')]");              // 'Участники'
+
+    // Ссылка - Итоговая аттестация (Перейти )
+    private SelenideElement goToLink              = $x("//td[contains(text(),'Итоговая аттестация')]/a");  // 'Перейти'
+
+    // Таблица
+    private SelenideElement fullNameOnTable       = $x("//th[contains(text(),'ФИО')]");                    // 'ФИО'
+    private SelenideElement statusTable           = $x("//th[contains(text(),'Статус')]");                 // 'Статус'
+    private SelenideElement resultsTable          = $x("//th[contains(text(),'Результат')]");              // 'Результат'
 
 }
