758b68672486fd7b1e385b9492ba35712637e163
RustamElkin <ryolkin90@gmail.com>
2021-02-16 14:56:56 +0600

    added to new pages xpath locators

--- src/main/java/ExamTicketGroupsCreatePage.java
+++ src/main/java/ExamTicketGroupsCreatePage.java
@@ -10,48 +10,51 @@ import static com.codeborne.selenide.Selenide.$x;
 public class ExamTicketGroupsCreatePage {
 
     // Тайтл
-    private SelenideElement titleToPage = $x("//h1[contains(text(),'Создание группы экзаменационных билетов')]");
+    private SelenideElement titleToPage                 = $x("//h1[contains(text(),'Создание группы экзаменационных билетов')]");
 
     // Инпуты.  Вбить свое или сделать клик чтоб выбрать из списка
-    private SelenideElement nameInput = $x("//label[contains(text(),'Название')]/following-sibling::input");                                // 'Название'
-    private SelenideElement subjectsInput = $x("//label[contains(text(),'Предмет')]/following-sibling::div//input");                        // 'Предмет'
-    private SelenideElement languageEducationInput = $x("//label[contains(text(),'Язык обучения')]/following-sibling::div//input");         // 'Язык обучения'
-    private SelenideElement additionalTeacherInput = $x("//label[contains(text(),'Доп. преподаватели')]/following-sibling::div//input");    // 'Доп. преподаватели'
+    private SelenideElement nameInput                   = $x("//label[contains(text(),'Название')]/following-sibling::input");                                         // 'Название'
+    private SelenideElement subjectsInput               = $x("//label[contains(text(),'Предмет')]/following-sibling::div//input");                                     // 'Предмет'
+    private SelenideElement languageEducationInput      = $x("//label[contains(text(),'Язык обучения')]/following-sibling::div//input");                               // 'Язык обучения'
+    private SelenideElement additionalTeacherInput      = $x("//label[contains(text(),'Доп. преподаватели')]/following-sibling::div//input");                          // 'Доп. преподаватели'
 
+    // dropdown list. Кликнув на div в котором завернут инпут, выпадает список
+    private SelenideElement subjectsDropdown            = $x("//label[contains(text(),'Предмет')]/following-sibling::div[@class='multiselect mb-10']");                // 'Предмет'
+    private SelenideElement languageEducationDropdown   = $x("//label[contains(text(),'Язык обучения')]/following-sibling::div[@class='multiselect mb-10']");          // 'Язык обучения'
+    private SelenideElement additionalTeacherDropdown   = $x("//label[contains(text(),'Доп. преподаватели')]/following-sibling::div[@class='multiselect mb-10']");     // 'Доп. преподаватели'
 
-    // dropdown list. Кликнув на инпут выпадают списки
 
     // Список предметов в div[3], выбрать предмет можно указав li[] или по тексту в span[contains(text(),'Казахский язык')]
     // Если предмет повторяется, лучше указывать li[] или брать все в скобки и добавив квадратные, в них указывать индекс
 
     // Список предметов.    В пропертях li есть innerText: "Русский язык ", textContent: "Русский язык ". Фильтровать используя проперти.
-    private ElementsCollection subjectList = $$x("//form/div[3]//li");
+    private ElementsCollection subjectList              = $$x("//label[contains(text(),'Предмет')]/following-sibling::div[@class='multiselect mb-10']//span/span");
 
     // Выбор определенного предмета
-    private SelenideElement selectSubjectsInDropdown = $x("//form/div[3]//li[1]/span/span[contains(text(),'Казахский язык')]");
+    private SelenideElement selectSubjectsInDropdown    = $x("//label[contains(text(),'Предмет')]/following-sibling::div[@class='multiselect mb-10']//span/span[contains(text(),'Казахский язык')]");
 
     // Список 'Язык обучения'
-    private ElementsCollection languagesEducationList = $$x("//form/div[4]//li");
+    private ElementsCollection languagesEducationList   = $$x("//label[contains(text(),'Язык обучения')]/following-sibling::div[@class='multiselect mb-10']//span/span");
 
     // Выбор определенного языка обучения
-    private SelenideElement selectLanguageEducationInDropdown = $x("//form/div[4]//li[5]/span/span[contains(text(),'корейский')]");
+    private SelenideElement selectLanguageEducationInDropdown = $x("//label[contains(text(),'Язык обучения')]/following-sibling::div[@class='multiselect mb-10']//span/span[contains(text(),'русский')]");
 
     // Список 'Доп. преподаватели'
-    private ElementsCollection additionalTeachersList = $$x("//form/div[5]//li");
+    private ElementsCollection additionalTeachersList   = $$x("//label[contains(text(),'Доп. преподаватели')]/following-sibling::div[@class='multiselect mb-10']//span/span");
 
     // Выбор определенного преподавателя
-    private SelenideElement selectAdditionalTeacherInDropdown = $x("//form/div[5]//li[1]/span/span[contains(text(),'Алтаев Абит Уристемович')]");
+    private SelenideElement selectAdditionalTeacherInDropdown = $x("//label[contains(text(),'Доп. преподаватели')]/following-sibling::div[@class='multiselect mb-10']//span/span[contains(text(),'Алтаев Абит Уристемович')]");
 
 
     // Валидация error message (на деве нет переводов. чекнуть перед написанием тестов)
-    private SelenideElement emptyNameInput = $x("//div[contains(text(),'Название\" обязательно для заполнения')]");
-    private SelenideElement emptySubjectsInput = $x("//div[contains(text(),'Дисциплина\" обязательно для заполнения')]");
-    private SelenideElement emptyLanguageEducationInput = $x("//div[contains(text(),'Язык обучения\" должно быть целым числом')]");
+    private SelenideElement emptyNameInput              = $x("//div[contains(text(),'Название')]");
+    private SelenideElement emptySubjectsInput          = $x("//div[contains(text(),'Дисциплина')]");
+    private SelenideElement emptyLanguageEducationInput = $x("//div[contains(text(),'Язык обучения')]");
 
 
     // Кнопки
-    private SelenideElement cancelBtn= $x("//div[@class='col-12 col-sm-auto mb-20']/a[contains(text(),'Отмена')]");
-    private SelenideElement saveBtn= $x("//span[contains(text(),'Сохранить')]/..");
+    private SelenideElement cancelBtn                   = $x("//div[@class='col-12 col-sm-auto mb-20']/a[contains(text(),'Отмена')]");
+    private SelenideElement saveBtn                     = $x("//span[contains(text(),'Сохранить')]/..");
 
 
 }
--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -47,7 +47,4 @@ public class MainPage {
         return this;
     }
 
-
-
-
 }
