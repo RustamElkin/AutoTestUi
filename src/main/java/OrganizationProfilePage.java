33e7b49d568ae0b1f64acfe3c7c3731628c91b7c
RustamElkin <ryolkin90@gmail.com>
2021-02-17 12:30:21 +0600

    added to new pages xpath locators

--- src/main/java/OrganizationProfilePage.java
+++ src/main/java/OrganizationProfilePage.java
@@ -8,19 +8,19 @@ import static com.codeborne.selenide.Selenide.$x;
 public class OrganizationProfilePage {
 
     // Тайтл
-    private SelenideElement titleToPage = $x("//h1[contains(text(),'Профиль колледжа')]");
+    private SelenideElement titleToPage                 = $x("//h1[contains(text(),'Профиль колледжа')]");
 
     // Табы на странице
-    private SelenideElement organizationTab = $x("//span[contains(text(),'Организация')]/..");                                   // 'Организация'
-    private SelenideElement registrationInformationTab = $x("//span[contains(text(),'Регистрационные сведения')]/..");           // 'Регистрационные сведения'
-    private SelenideElement materialAndTechnicalBaseTab = $x("//span[contains(text(),'МТБ')]/..");                               // 'МТБ'
+    private SelenideElement organizationTab             = $x("//span[contains(text(),'Организация')]/..");                        // 'Организация'
+    private SelenideElement registrationInformationTab  = $x("//span[contains(text(),'Регистрационные сведения')]/..");           // 'Регистрационные сведения'
+    private SelenideElement materialAndTechnicalBaseTab = $x("//span[contains(text(),'МТБ')]/..");                                // 'МТБ'
 
     // Кнопка "Редактировать"
-    private SelenideElement editBtn = $x("//div[contains(text(),'Редактировать')]/..");
+    private SelenideElement editBtn                     = $x("//div[contains(text(),'Редактировать')]/..");
 
     // Можно чекнуть наличие titl-ов "Описание и Адрес"
-     private SelenideElement descriptionTitle = $x("//span[contains(text(),'Описание')]");
-     private SelenideElement addressTitle = $x("//span[contains(text(),'Адрес')]");
+     private SelenideElement descriptionTitle           = $x("//span[contains(text(),'Описание')]");
+     private SelenideElement addressTitle               = $x("//span[contains(text(),'Адрес')]");
 
 
 
--- src/main/java/ViewHomeworkTemplatePage.java
+++ src/main/java/ViewHomeworkTemplatePage.java
@@ -1,8 +1,41 @@
 // Дистанционное обучение_Просмотр шаблона задания
-//  /ru/tko/control/homeworks/230
+//  /ru/tko/control/homeworks/1369871
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class ViewHomeworkTemplatePage {
 
+    // Тайтл
+    private SelenideElement titleToPage                        = $x("//h1[contains(text(),'Шаблоны заданий')]");
+
+    // Табы на странице
+    private SelenideElement backButtonTab                      = $x("//span[contains(text(),'Назад')]/..");             // 'Назад'
+
+    // Тайтлы на странице
+    private SelenideElement legalRepresentativeInformation     = $x("//span[contains(text(),'Лекция')]");               // 'Сведения о законном представителе'
+    private SelenideElement applicantInformation               = $x("//span[contains(text(),'Задание')]");              // 'Сведения об абитуриенте'
+
+    // Лекция - Вложенные файлы
+    private ElementsCollection lectureAttachmentsFilesView     = $$x("//span[contains(text(),'Лекция')]/ancestor::div[@class='profile-content__block']//button");
+
+    // Задание - Вложенные файлы
+    private ElementsCollection homeworkAttachmentsFilesView    = $$x("//span[contains(text(),'Задание')]/ancestor::div[@class='profile-content__block']//button");
+
+    // image-viewer
+    private SelenideElement viewerZoomIn                       = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-zoom-in']");
+    private SelenideElement viewerZoomOut                      = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-zoom-out']");
+    private SelenideElement viewerOneToOne                     = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-one-to-one']");
+    private SelenideElement viewerReset                        = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-reset']");
+    private SelenideElement viewerFullLarge                    = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-play viewer-large']");
+    private SelenideElement viewerRotateLeft                   = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-rotate-left']");
+    private SelenideElement viewerRotateRight                  = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-rotate-right']");
+    private SelenideElement viewerFlipHorizontal               = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-flip-horizontal']");
+    private SelenideElement viewerFlipVertical                 = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//li[@class = 'viewer-flip-vertical']");
+    private SelenideElement viewerClose                        = $x("//div[contains(@class,'viewer-fade') and @id = 'viewer0']//div[contains(@class,'viewer-close') and @role='button']");
 
 
 }
