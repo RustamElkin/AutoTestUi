89af0d6e23c58c5c6696c530fcfe59c4ad1df701
RustamElkin <ryolkin90@gmail.com>
2021-02-18 17:50:08 +0600

    added to new pages xpath locators

--- src/main/java/AnnualArchiveModulesJournalsPage.java
+++ src/main/java/AnnualArchiveModulesJournalsPage.java
@@ -18,8 +18,13 @@ public class AnnualArchiveModulesJournalsPage {
     private SelenideElement resetBtn = $x("//span[contains(text(),'Сбросить')]");
     //private SelenideElement resetBtn = $("form div:nth-child(2) > button");
     private SelenideElement firstLookAtBtn = $("table tr:nth-child(1) a");
-    private SelenideElement backPaginateBtn = $x("//a[contains(text(),'‹')]");
-    private ElementsCollection selectPaginateBtn = $$x("//ul[@role='navigation']//a");
-    private SelenideElement nextPaginateBtn = $x("//a[contains(text(),'›')]");
 
+    // Кнопки пагинации на странице
+    private SelenideElement backPaginateBtn         = $x("//a[contains(text(),'‹')]");  // Не работает на текущей странице, кнопка не активна, тег "a" отображается как span
+    private SelenideElement firstPaginateBtn        = $x("//a[contains(text(),'1')]");
+    private SelenideElement secondPaginateBtn       = $x("//a[contains(text(),'2')]");
+    private SelenideElement thirdPaginateBtn        = $x("//a[contains(text(),'3')]");
+    private SelenideElement fourthPaginateBtn       = $x("//a[contains(text(),'4')]");
+    private ElementsCollection selectPaginateBtn    = $$x("//ul[@role = 'navigation']//a");
+    private SelenideElement nextPaginateBtn         = $x("//a[contains(text(),'›')]");
 }
--- src/main/java/ViewAnnualArchiveJournalPage.java
+++ src/main/java/ViewAnnualArchiveJournalPage.java
@@ -1,8 +1,34 @@
 // Архив_Журнал
 //  /ru/tko/control/annual-archive/modules/4/journals/709/show
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ViewAnnualArchiveJournalPage {
 
+    // Тайтл
+    private SelenideElement titleToPage                     = $x("//h1[contains(text(),'Журнал')]");
 
+    // Табы на странице
+    private SelenideElement backTab                         = $x("//span[contains(text(),'Назад')]/..");                                                             // 'Назад'
+    private SelenideElement recoveryTab                     = $x("//span[contains(text(),'Восстановить')]/..");                                                      // 'Восстановить'
 
+    // Кнопки
+    private SelenideElement exportBtn                       = $x("//a[contains(text(),'Экспортировать')]");                                                          // кнопка 'Экспортировать'
+
+    // Dropdown elements | фильтровать по названию или по индексу
+    private SelenideElement subjectDropdown                 = $x("//select[@name = 'discipline']/parent::div[@class = 'button-journal-menu']");
+    private ElementsCollection subjectList                  = $$x("//select[@name = 'discipline']/option");
+
+    // Модалка появится если нажать на кнопку "Восстановить"
+    // Кнопки в модалке
+    private SelenideElement closeModalBtn                   = $x("(//div[@data-target = '#modal-control-confirm-js' and @class='s-modal__close'])[1]");              // 'Закрыть модалку'
+    private SelenideElement submitBtn                       = $x("(//a[text() ='Отмена' and @data-target='#modal-control-confirm-js']/following-sibling::a)[1]");    // 'Подтвердить'
+    private SelenideElement cancelBtn                       = $x("(//a[text() ='Отмена' and @data-target='#modal-control-confirm-js'])[1]");                         // 'Отмена'
+
+    // При поптыке экспортировать пустой журнал, валидация
+    private SelenideElement emptyJournalNotificationMessage =$x("//div[@class='s-notification__message' and text() = 'Нет данных']");
 }
